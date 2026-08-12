e759d049a55be963577fdb7b801ef72a2f29a3b0
yunusrf <yunusrf@gmail.com>
2026-08-12 12:41:43 +0300

    Update insert_document.py

--- DSCI-2333-AdvCodDB_AIDS/week14/src/insert_document.py
+++ DSCI-2333-AdvCodDB_AIDS/week14/src/insert_document.py
@@ -1,6 +1,6 @@
 from astrapy import DataAPIClient
-API_ENDPOINT = "https://1033fe26-1e46-4c5a-9dba-7eb987b8ea93-us-east-2.apps.astra.datastax.com"
-APPLICATION_TOKEN = "AstraCS:HDTWhqOfCIBaDDKmOhSzErAU:a988ad29b66203fad11808e6b6429d377cb0bfd94be89286957e112c2bbc5e33"
+API_ENDPOINT = ""
+APPLICATION_TOKEN = ""
 
 # Get an existing collection
 client = DataAPIClient()
